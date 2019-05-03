5ff908ef646923f7839322910579e2676b332a9f
kumatake-it-slow <kuma1.23g@gmail.com>
2019-05-03 17:37:18 +0900

    new create 完成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,4 +2,20 @@ class BooksController < ApplicationController
   def index
     @books = Book.all
   end
+
+  def new
+    @book = Book.all
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to @book, notice:"書籍を登録しました。"
+  end
+
+Private
+
+  def book_params
+    params.require(:book).permit(:title, :price, :publish_date, :description)
+  end
 end
